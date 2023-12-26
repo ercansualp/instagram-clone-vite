1b866d2ab208a28d97dee5b21b32fb46fdfa37ac
Ercan SUALP <ercansualp@outlook.com.tr>
2023-12-26 12:27:09 +0300

    fadsfs

--- server/php/index.php
+++ server/php/index.php
@@ -1,8 +1,11 @@
 <?php
+echo "fsadfdsa";
+/*
 	try {
 		$this->conn = new PDO("mysql:host=13.53.159.183:3306;dbname=instagram-clone", "root", "123456");
 		echo "ercannnn";
 	} catch (PDOException $e) {
 		print $e->getMessage();
 	}
+	*/
 ?>
