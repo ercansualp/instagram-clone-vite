2c6ea8f60bded2e303b94677854a88953891dad0
Ercan SUALP <ercansualp@outlook.com.tr>
2023-12-26 10:52:02 +0300

    safdsadf

--- client/src/url.js
+++ client/src/url.js
@@ -1,5 +1,7 @@
-const backEndUrl = "http://api:80";
+const backEndUrl = "http://api:8080";
 const managerFolderUrl = backEndUrl + "/manager";
 
 export const postApi = managerFolderUrl + "/PostManager.php";
 export const authApi = managerFolderUrl + "/AuthenticationManager.php";
+
+// http://13.50.130.221:8080/manager/AuthenticationManager.php
